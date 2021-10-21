ec4147c80fcc03c2d1aad8c6a154d19cd09da987
Fernanda Nogueira <fernanda.canto.ngr@gmail.com>
2021-10-21 16:27:34 -0300

    feat: initial commit

---
+++ plugins/components.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Page from '~/components/Page.vue'
+import Teaser from '~/components/Teaser.vue'
+import Grid from '~/components/Grid.vue'
+import Feature from '~/components/Feature.vue'
+ 
+Vue.component('Page', Page)
+Vue.component('Teaser', Teaser)
+Vue.component('Grid', Grid)
+Vue.component('Feature', Feature)
